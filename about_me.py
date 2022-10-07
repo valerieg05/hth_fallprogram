b82070a87ff97bbc073769855ea1dff7d5c37b0e
valerieg05 <valerie.gutierrezcastillo@gmail.com>
2022-10-06 18:58:47 -0700

    hth class 10/06/2022

---
+++ about_me.py
@@ -0,0 +1,12 @@
+print('Hello!')
+name= "Valerie Gutierrez"
+nickname= "Valerie"
+age= 17
+python= False
+myHobbies=["painting","baking","gardening","doing nails"]
+favs = {
+  "movie": "Nightmare before Christmas",
+  "food": "Pozole",
+  "hobby": "baking"
+}
+all_vars = dict(vars())
